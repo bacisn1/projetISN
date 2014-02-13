bc234ffae118ede16b8096c0afac723d1bddc441
Thibaud-Boquet <isn@usbkisn.isn.solidev.net>
2014-02-13 15:31:05 +0100

    Ajout un fichier python

---
+++ bouclefor.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 28 15:28:55 2013
+
+@author: isn
+"""
+
+semaine=['lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche']
+for jour in semaine:
+    print(jour)    
